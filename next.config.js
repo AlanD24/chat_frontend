153e775be9d5cf6cc7dd9edfd1a242047332301a
aland <alan.danjr@gmail.com>
2023-10-11 11:35:15 -0600

    api url for vercel backend

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     reactStrictMode: false,
     env: {
-        NEXT_APP_API_URL: "http://localhost:8080/api"
+        NEXT_APP_API_LOCAL: "http://localhost:8080/api",
+        NEXT_APP_API_URL: "https://chat-backend-phi.vercel.app:8080/api"
     },
     images: {
         domains: ['localhost']
